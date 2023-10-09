b55499c3d73899824cd1405b8fd80c2366895a10
fhasancelik <hasancelikjob@gmail.com>
2023-10-10 00:05:53 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <header className="d-flex w-100 align-items-center flex-wrap justify-content-between px-4 py-3 bg-light text-black ">
+      <div>E-Commerce</div>
+
+      <div className="d-flex gap-4">
+        <Link className="btn btn-primary" to={"/"}>
+          AnaSayfa
+        </Link>
+
+        <Link className="btn btn-warning" to={"/basket"}>
+          Sepet
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
